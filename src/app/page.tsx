f7b67bb86459d43201beb8fff87b1730229eaa2d
caru-ini <caru17122@gmail.com>
2024-07-23 00:27:19 +0900

    feat: add ideas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 import { useChat } from 'ai/react';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/monokai-sublime.css';
-import { ArrowUp, Bot, CircleStop, User } from 'lucide-react';
+import { ArrowUp, Bot, CircleStop, NotebookPen, PenLine, Terminal, User } from 'lucide-react';
 import markdownit from 'markdown-it';
 
 const md = markdownit({
@@ -23,23 +23,59 @@ const md = markdownit({
   },
 });
 
+const ideas = [
+  {
+    title: 'Create My Own Website',
+    prompt: 'Please help me create a website for my portfolio.',
+    icon: <Terminal />,
+  },
+  {
+    title: 'Create a Blog Article',
+    prompt: 'Can you help me write a blog article on the benefits of exercise?',
+    icon: <PenLine />,
+  },
+  {
+    title: 'Create a Short Story',
+    prompt: 'Can you help me write a short story about a detective solving a crime?',
+    icon: <NotebookPen />,
+  },
+  {
+    title: 'Create a Poem',
+    prompt: 'Can you help me write a poem about love and loss?',
+    icon: <PenLine />,
+  },
+];
+
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat({
-    initialMessages: [
-      {
-        id: '1',
-        role: 'assistant',
-        content: '```python\nprint("Hello, World!")\n```',
-      },
-    ],
-  });
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop, setInput } = useChat();
 
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <h1 className='text-3xl'>OpenAI-Practice</h1>
-      <p className='mt-1 text-lg text-muted-foreground'>Type something to get started.</p>
-      <ToggleTheme />
-      <div className='mx-auto my-10 flex min-w-[400px] max-w-full flex-col'>
+    <div className='flex min-h-screen flex-col items-center py-2'>
+      {!messages.length && (
+        <div id='welcome' className='fixed top-[45svh] z-10 grid gap-y-3'>
+          <div id='motd' className='rounded-md bg-secondary p-3'>
+            <h1 className='text-3xl'>OpenAI-Practice</h1>
+            <p className='mt-1 text-lg text-muted-foreground'>Type something to get started.</p>
+            <div>
+              <ToggleTheme />
+            </div>
+          </div>
+          <p className='text-xl text-muted-foreground'>Ideas</p>
+          <div className='grid grid-cols-2 gap-2'>
+            {ideas.map((idea, i) => (
+              <button
+                key={i}
+                className='flex flex-col gap-x-2 rounded-md border border-border p-3'
+                onClick={() => setInput(idea.prompt)}
+              >
+                <div className='mt-1 h-full rounded-md text-white'>{idea.icon}</div>
+                <p className='text-start text-xl'>{idea.title}</p>
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+      <div className='mx-auto my-16 flex min-w-[400px] max-w-[800px] flex-col px-2'>
         <div className='rounded-md bg-secondary'>
           {messages.map((message, i) => (
             <div key={i} className='flex gap-x-2 p-2'>
@@ -54,7 +90,7 @@ export default function Home() {
               <div className='flex-1'>
                 <p className='text-xl'>{message.role}</p>
                 <div
-                  className='markdown-body mt-1'
+                  className='markdown-body mt-1 max-w-[95%]'
                   dangerouslySetInnerHTML={{ __html: md.render(message.content) }}
                 />
               </div>
